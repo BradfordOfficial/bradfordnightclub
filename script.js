961aa1eff3e42c2b1921ee7de3463fd9c092882e
bradford <mathys.gonzalez07@gmail.com>
2026-02-17 19:05:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4950,7 +4950,7 @@ function renderLocationPage() {
             </div>
 
             <footer class="location-footer">
-                <p>PRIVATE EVENTS & WORLDWIDE INQUIRIES: concierge@bradford.com</p>
+                <p>PRIVATE EVENTS & WORLDWIDE INQUIRIES: events@bradfordnightclub.com</p>
             </footer>
         </div>
     `;
