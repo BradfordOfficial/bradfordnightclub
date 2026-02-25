ec88edd74a1e1b4ac0d021d4af1a902d7addab48
bradford <mathys.gonzalez07@gmail.com>
2026-02-25 23:52:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4709,7 +4709,7 @@ const B_ENGINE = {
 
 
     // 3. Lancement des données générées (les 1000 messages du club)
-    this.generateData(1000); 
+    this.generateData(30000); 
 
     // --- CORRECTION POINT B : NETTOYAGE DES ANCIENS TESTS ---
     let savedMsgs = JSON.parse(localStorage.getItem("BRADFORD_MY_MSGS") || "[]");
